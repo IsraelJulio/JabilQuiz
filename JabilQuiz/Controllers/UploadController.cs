2f5603b68e9d7f22e25de9124f7770d168e7dcaf
Israel Julio <israel.jhonatas@sesisenaibetim.com.br>
2022-03-03 03:51:44 -0300

    Upload files

---
+++ JabilQuiz/Controllers/UploadController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+using NPOI.SS.UserModel;
+using System.IO;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using JabilQuiz.Model;
+
+namespace WebApplication.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> UploadFiles(IFormFile file)
+        {
+            try
+            {
+                ISheet sheet;
+                string fileName = Path.GetFileName(file.FileName);
+                var fileExt = Path.GetExtension(fileName);
+
+                if (fileExt == ".xls")
+                {
+                    HSSFWorkbook hssfwb = new HSSFWorkbook(file.OpenReadStream());
+                    sheet = hssfwb.GetSheetAt(0);
+                }
+                else
+                {
+                    XSSFWorkbook hssfwb = new XSSFWorkbook(file.OpenReadStream());
+                    sheet = hssfwb.GetSheetAt(0);
+                }
+                //throw new Exception($"METHOD NOT ALLOWED");             
+
+                try
+                {
+                    List<Quiz> QuizList = new List<Quiz>();
+                    var actualRow = sheet.GetRow(0);
+                    bool skip = false;
+                    
+                    for (int row = 0; (row <= sheet.LastRowNum) && (skip == false); row++)
+                    {
+                        actualRow = sheet.GetRow(row);
+                        if (actualRow.GetCell(0).StringCellValue == "")
+                            skip = true;
+                        else
+                        {
+                            Quiz quiz = new Quiz();
+                            quiz.Question = actualRow.GetCell(0).StringCellValue;
+                            quiz.Answer = actualRow.GetCell(1).StringCellValue;
+                            QuizList.Add(quiz);
+                        }
+                    }
+
+
+                    return Ok(QuizList);
+
+                }
+                catch (Exception e) when (e.Message.Contains("METHOD NOT ALLOWED"))
+                {
+                    return StatusCode(StatusCodes.Status405MethodNotAllowed, e.Message);
+                }
+                catch (Exception e) when (e.Message.Contains("Not Found"))
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, e.Message);
+                }
+                catch (Exception e)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.ToString());
+            }
+
+        }
+    }
+}
---
+++ JabilQuiz/Model/Quiz.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JabilQuiz.Model
+{
+    public class Quiz
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
